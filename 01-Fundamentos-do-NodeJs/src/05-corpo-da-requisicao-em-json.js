50010ffc4782db5b302087127d7d1221ec2d7eb9
Davi Ferreira <davi.rferreira@gmail.com>
2026-03-23 14:16:50 -0300

    Adicionando o corpo a requisição

---
+++ 01-Fundamentos-do-NodeJs/src/05-corpo-da-requisicao-em-json.js
@@ -0,0 +1,40 @@
+import http from 'node:http'
+
+const users = []
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req
+
+  console.log(method, url)
+
+  const buffers = []
+
+  for await (const chunck of req) {
+    buffers.push(chunck)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
+
+  if (method === 'GET' && url === '/users') {
+    return res.setHeader('Content-Type', 'aplication/json').end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
+    users.push({
+      id: 1,
+      name,
+      email
+    })
+    return res.writeHead(201).end("Criando um usuário")
+  }
+
+  return res.writeHead(404).end(`Hello, World!\n\nMetodo: ${method}\nUrl: ${url}`);
+});
+
+server.listen(3333);
