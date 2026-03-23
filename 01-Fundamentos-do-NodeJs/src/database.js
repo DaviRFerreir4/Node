e10754b605a4ec0e92928757464ff844ad6b087a
Davi Ferreira <davi.rferreira@gmail.com>
2026-03-23 15:01:52 -0300

    Persistindo dados num arquivo de banco de dados

--- 01-Fundamentos-do-NodeJs/src/database.js
+++ 01-Fundamentos-do-NodeJs/src/database.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises'
+
+const DATABASE_PATH = new URL('../db.json', import.meta.url)
+
 export class Database {
   #database = {}
+
+  constructor() {
+    fs.readFile(DATABASE_PATH, 'utf-8').then((data) => {
+      this.#database = JSON.parse(data)
+    }).catch(() => {
+      this.#persist()
+    })
+  }
+
+  #persist() {
+    fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))
+  }
 
   select(table) {
     const data = this.#database[table] ?? []
@@ -13,6 +29,8 @@ export class Database {
     } else {
       this.#database[table] = [data]
     }
+
+    this.#persist()
 
     return data
   }
