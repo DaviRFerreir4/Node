bb0cd558c8370ae4dab88166993be4aea8d27f2b
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-08 01:22:51 -0300

    02-Criando-API-REST-moderna-com-NodeJs | 03-Testes-Automatizados - Teste para listar transações

--- 02-Criando-API-REST-moderna-com-NodeJs/test/transactions.spec.ts
+++ 02-Criando-API-REST-moderna-com-NodeJs/test/transactions.spec.ts
@@ -2,7 +2,7 @@ import { expect, beforeAll, describe, it, afterAll } from 'vitest'
 import { app } from '../src/app'
 import request from 'supertest'
 
-describe('transactions', () => {
+describe('Transactions Routes', () => {
   beforeAll(async () => {
     await app.ready()
   })
@@ -11,11 +11,30 @@ describe('transactions', () => {
     await app.close()
   })
 
-  it('should let the user create a new request', async () => {
+  it('should be able to create a new transaction', async () => {
     const response = await request(app.server)
       .post('/transactions')
       .send({ title: 'Test Transaction', amount: 5000, type: 'credit' })
 
     expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({ title: 'Test Transaction', amount: 5000, type: 'credit' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const response = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies ?? [])
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      transactions: [
+        expect.objectContaining({ title: 'Test Transaction', amount: 5000 }),
+      ],
+    })
   })
 })
