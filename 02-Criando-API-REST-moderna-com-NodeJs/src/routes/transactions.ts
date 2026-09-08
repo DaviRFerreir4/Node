c5108d1a4f2c14cb58f021d3a3d41bb7378d26a7
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-07 23:35:56 -0300

    02-Criando-API-REST-moderna-com-NodeJs | 02-Implementando-Rotas - Validando o session_id com um preHandler (middleware)

--- 02-Criando-API-REST-moderna-com-NodeJs/src/routes/transactions.ts
+++ 02-Criando-API-REST-moderna-com-NodeJs/src/routes/transactions.ts
@@ -2,26 +2,32 @@ import type { FastifyInstance } from 'fastify'
 import { knex } from '../database.ts'
 import z from 'zod'
 import { randomUUID } from 'node:crypto'
+import { checkSessionIdExists } from '../middlewares/checkSessionId.ts'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions')
+  app.get('/', { preHandler: checkSessionIdExists }, async (request) => {
+    const transactions = await knex('transactions').where({
+      session_id: request.sessionId ?? '',
+    })
 
     return { transactions }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: checkSessionIdExists }, async (request) => {
     const paramsSchema = z.object({ id: z.uuid() })
 
     const { id } = paramsSchema.parse(request.params)
 
-    const transaction = await knex('transactions').where({ id }).first()
+    const transaction = await knex('transactions')
+      .where({ id, session_id: request.sessionId ?? '' })
+      .first()
 
     return { transaction }
   })
 
-  app.get('/summary', async (request) => {
+  app.get('/summary', { preHandler: checkSessionIdExists }, async (request) => {
     const summary = await knex('transactions')
+      .where({ session_id: request.sessionId ?? '' })
       .sum('amount', { as: 'amount' })
       .first()
 
@@ -54,6 +60,6 @@ export async function transactionsRoutes(app: FastifyInstance) {
       session_id: sessionId,
     })
 
-    return reply.code(201).send()
+    return reply.status(201).send()
   })
 }
