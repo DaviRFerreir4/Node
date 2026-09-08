f18a98815031bf1f0c300093351f55f3511a8db4
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-07 23:03:20 -0300

    02-Criando-API-REST-moderna-com-NodeJs | 02-Implementando-Rotas - Tipagem no knex

--- 02-Criando-API-REST-moderna-com-NodeJs/src/database.ts
+++ 02-Criando-API-REST-moderna-com-NodeJs/src/database.ts
@@ -1,7 +1,7 @@
-import knexConfig from 'knex'
+import { knex as knexConfig, Knex } from 'knex'
 import { env } from './env/index.ts'
 
-export const configs: knexConfig.Knex.Config = {
+export const configs: Knex.Config = {
   client: 'better-sqlite3',
   connection: {
     filename: env.DATABASE_URL,
