a67464be98d4f848b729a7b1a5c817bbd3f6199a
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-09 00:52:18 -0300

    03-API-NodeJs-com-SOLID | 03-Casos-de-Uso-e-Design-Patterns - Lidando com erros de forma global

--- 03-API-NodeJs-com-SOLID/src/app.ts
+++ 03-API-NodeJs-com-SOLID/src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes.ts'
+import z, { ZodError } from 'zod'
+import { env } from './env/index.ts'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((error, _, reply) => {
+  if (error instanceof ZodError) {
+    return reply
+      .status(400)
+      .send({ message: 'Validation Error', issues: z.treeifyError(error) })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO enviar o log para uma ferramenta externa como DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ message: 'Internal Server Error' })
+})
--- 03-API-NodeJs-com-SOLID/src/http/controllers/users.ts
+++ 03-API-NodeJs-com-SOLID/src/http/controllers/users.ts
@@ -8,7 +8,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const bodySchema = z.object({
     name: z.string(),
     email: z.email(),
-    password: z.string(),
+    password: z.string().min(8),
   })
 
   const { name, email, password } = bodySchema.parse(request.body)
@@ -23,7 +23,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
       return reply.status(409).send({ message: error.message })
     }
 
-    return reply.status(500).send()
+    throw error
   }
 
   return reply.status(201).send()
