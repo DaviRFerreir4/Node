a0b22165fa8226a77ab95bc2462069816c3dff2d
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-08 21:44:38 -0300

    03-API-NodeJs-com-SOLID | 02-Integracao-com-Prisma - Alterando schema e gerando o client

--- 03-API-NodeJs-com-SOLID/src/app.ts
+++ 03-API-NodeJs-com-SOLID/src/app.ts
@@ -1,3 +1,10 @@
 import fastify from 'fastify'
+import { PrismaClient } from '../generated/prisma/client.ts'
+import { PrismaPg } from '@prisma/adapter-pg'
+import { env } from './env/index.ts'
 
 export const app = fastify()
+
+const prisma = new PrismaClient({ adapter: new PrismaPg(env.DATABASE_URL) })
+
+prisma.user.create({ data: { name: 'Teste', email: 'teste@email.com' } })
