cd2f37f54dcfbc7e9cf415782590f46df5f9bdc4
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-09 00:23:17 -0300

    03-API-NodeJs-com-SOLID | 03-Casos-de-Uso-e-Design-Patterns - Aplicando inversão de dependência no use case de user

---
+++ 03-API-NodeJs-com-SOLID/solid.md
@@ -0,0 +1,6 @@
+# SOLID
+
+## D (Dependency Inversion Principle)
+
+- Inverte a ordem em que a dependência chega no caso de uso
+- Ex: ao invés de instanciar uma dependência, você recebe ela como parâmetro
--- 03-API-NodeJs-com-SOLID/src/use-cases/users.ts
+++ 03-API-NodeJs-com-SOLID/src/use-cases/users.ts
@@ -1,7 +1,5 @@
 import { prisma } from '@/lib/prisma.ts'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository.ts'
 import { hash } from 'bcryptjs'
-import type { FastifyReply, FastifyRequest } from 'fastify'
 
 interface RegisterUseCaseRequest {
   name: string
@@ -9,20 +7,18 @@ interface RegisterUseCaseRequest {
   password: string
 }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
 
-  if (userWithSameEmail) {
-    throw new Error('Email already registred')
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const userWithSameEmail = await prisma.user.findUnique({ where: { email } })
+
+    if (userWithSameEmail) {
+      throw new Error('Email already registred')
+    }
+
+    const password_hash = await hash(password, 6)
+
+    await this.usersRepository.create({ name, email, password_hash })
   }
-
-  const password_hash = await hash(password, 6)
-
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({ name, email, password_hash })
 }
