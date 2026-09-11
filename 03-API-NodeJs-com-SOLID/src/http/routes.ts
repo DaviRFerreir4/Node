0399965b7374396418659916dc761be4dd134143
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-11 11:03:29 -0300

    03-API-NodeJs-com-SOLID | 05-Implementando-Use-Cases - JWT no Fastify

--- 03-API-NodeJs-com-SOLID/src/http/routes.ts
+++ 03-API-NodeJs-com-SOLID/src/http/routes.ts
@@ -1,9 +1,12 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register.ts'
 import { authenticate } from './controllers/authenticate.ts'
+import { profile } from './controllers/profile.ts'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  // Authenticated
+  app.get('/me', profile)
 }
