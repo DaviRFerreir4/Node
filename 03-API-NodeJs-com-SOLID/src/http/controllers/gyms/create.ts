de378dedb5a03239c672505b3a9a0c0103d1f3a3
Davi Ferreira <davi.rferreira@gmail.com>
2026-09-17 11:54:53 -0300

    03-API-NodeJs-com-SOLID | 05-Implementando-Use-Cases - Restante dos controllers e rotas de academias

--- 03-API-NodeJs-com-SOLID/src/http/controllers/gyms/create.ts
+++ 03-API-NodeJs-com-SOLID/src/http/controllers/gyms/create.ts
@@ -2,13 +2,13 @@ import type { FastifyReply, FastifyRequest } from 'fastify'
 import z from 'zod'
 import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case.ts'
 
-export async function createGym(request: FastifyRequest, reply: FastifyReply) {
+export async function create(request: FastifyRequest, reply: FastifyReply) {
   const bodySchema = z.object({
     name: z.string(),
     description: z.string().nullable(),
     phone: z.string().nullable(),
-    latitude: z.coerce.number().refine((val) => Math.abs(val) <= 90),
-    longitude: z.coerce.number().refine((val) => Math.abs(val) <= 180),
+    latitude: z.number().refine((val) => Math.abs(val) <= 90),
+    longitude: z.number().refine((val) => Math.abs(val) <= 180),
   })
 
   const { name, description, phone, latitude, longitude } = bodySchema.parse(
---
+++ 03-API-NodeJs-com-SOLID/src/http/controllers/gyms/nearby.ts
@@ -0,0 +1,23 @@
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import z from 'zod'
+import { makeFetchNearbyGymsUseCase } from '@/use-cases/factories/make-fetch-nearby-gyms-use-case.ts'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const querySchema = z.object({
+    latitude: z.coerce.number().refine((val) => Math.abs(val) <= 90),
+    longitude: z.coerce.number().refine((val) => Math.abs(val) <= 180),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { latitude, longitude, page } = querySchema.parse(request.query)
+
+  const fetchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await fetchNearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+    page,
+  })
+
+  return reply.status(200).send(gyms)
+}
